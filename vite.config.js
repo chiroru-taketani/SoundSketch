095e5bba64f3d5c5c82b98eb0be7eef9ef4397fa
chiroru-taketani <chirorutaketani@gmail.com>
2026-03-11 20:57:58 +0900

    feat: setup app for PWA and local testing

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // ネットワーク上の他のデバイス（iPhoneなど）からのアクセスを許可
+  }
 })
